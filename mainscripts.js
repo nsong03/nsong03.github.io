2ea7369c59222954d71598c68dd4ec53e25a93ca
nsong03 <nathansong@berkeley.edu>
2023-05-27 10:13:32 -0700

    e

--- mainscripts.js
+++ mainscripts.js
@@ -5,7 +5,14 @@ var sectionHeaders = document.querySelectorAll('.toggle-header');
 sectionHeaders.forEach(function(header) {
   header.addEventListener('click', function() {
     var content = this.nextElementSibling;
-    content.style.display = (content.style.display === 'none') ? 'block' : 'none';
-    this.querySelector('.toggle-icon').textContent = (content.style.display === 'none') ? '▼' : '▲';
+    content.classList.toggle('show');
+    var isVisible = content.classList.contains('show');
+    this.querySelector('.toggle-icon').textContent = isVisible ? '▲' : '▼';
   });
+
+  // Set initial toggle button text based on display state
+  var content = header.nextElementSibling;
+  var isVisible = getComputedStyle(content).display !== 'none';
+  var toggleIcon = header.querySelector('.toggle-icon');
+  toggleIcon.textContent = isVisible ? '▲' : '▼';
 });
