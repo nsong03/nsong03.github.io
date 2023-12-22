e5680ea158e814ed65d892fa8ef2f0b685c9b606
nsong03 <85597680+nsong03@users.noreply.github.com>
2023-12-21 16:35:44 -0800

    update

--- mainscripts.js
+++ mainscripts.js
@@ -5,10 +5,10 @@ var sectionHeaders = document.querySelectorAll('.toggle-header');
 sectionHeaders.forEach(function(header) {
   header.addEventListener('click', function() {
     var content = this.nextElementSibling;
-    content.style.display = (content.style.display === 'none') ? 'block' : 'none';
-    this.querySelector('.toggle-icon').textContent = (content.style.display === 'none') ? '▼' : '▲';
+    content.classList.toggle('show');
+    var isVisible = content.classList.contains('show');
+    this.querySelector('.toggle-icon').textContent = isVisible ? '▲' : '▼';
   });
-<<<<<<< HEAD
 
   // Set initial toggle button text based on display state
   var content = header.nextElementSibling;
@@ -16,7 +16,3 @@ sectionHeaders.forEach(function(header) {
   var toggleIcon = header.querySelector('.toggle-icon');
   toggleIcon.textContent = isVisible ? '▲' : '▼';
 });
-
-=======
-});
->>>>>>> parent of 2ea7369 (e)
